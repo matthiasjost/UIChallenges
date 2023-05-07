212395c59b013208d2d8c1339bf4ceb6282414ed
Matthias Jost <8178852+matthiasjost@users.noreply.github.com>
2023-05-07 10:35:34 +0200

    fix layout

--- AgeCalculatorApp/AgeCalculatorApp/src/components/AgeCalculator.vue
+++ AgeCalculatorApp/AgeCalculatorApp/src/components/AgeCalculator.vue
@@ -1,5 +1,5 @@
-<script setup lang="ts">
-import { ref } from 'vue'
+<script lang="ts" setup>
+import {ref} from 'vue'
 
 const InputDays = ref("");
 const InputMonths = ref("");
@@ -9,56 +9,66 @@ const CalculatedDays = ref("");
 const CalculatedMonths = ref("");
 const CalculatedYears = ref("");
 
-function onSubmitForm()
-{
-  CalculatedDays.value = InputDays.value;
-  CalculatedMonths.value = InputMonths.value;
-  CalculatedYears.value = InputYears.value;
-  InputDays.value = "";
-  InputMonths.value = "";
-  InputYears.value = "";
+const DaysValidationError = ref("Error");
+const MonthsValidationError = ref("");
+const YearsValidationError = ref("");
+
+function onSubmitForm() {
+    CalculatedDays.value = InputDays.value;
+    CalculatedMonths.value = InputMonths.value;
+    CalculatedYears.value = InputYears.value;
+    InputDays.value = "";
+    InputMonths.value = "";
+    InputYears.value = "";
+
+    DaysValidationError.value = "Error";
+    MonthsValidationError.value = "Error";
+    YearsValidationError.value = "Error";
 }
 
 </script>
 
 <template>
-  <div id="main-container">
-    <div id="main-card">
-      <div class="grid">
-        <div class="grid-row-1-col-1">
-          <div class="form-combo">
-            <label for="days">Day</label>
-            <input v-model="InputDays" type="number" id="days"/>
-          </div>
+    <div id="main-container">
+        <div id="main-card">
+            <div class="grid">
+                <div class="grid-row-1-col-1">
+                    <div class="form-combo">
+                        <label class="input-labels" for="days">Day</label>
+                        <input id="days" v-model="InputDays" type="number"/>
+                        <span id="daysValidationError" class="validation-error">{{ DaysValidationError }}</span>
+                    </div>
+                </div>
+                <div class="grid-row-1-col-1">
+                    <div class="form-combo">
+                        <label class="input-labels" for="months">Month</label>
+                        <input id="months" v-model="InputMonths" type="number"/>
+                        <span id="monthsValidationError" class="validation-error">{{ MonthsValidationError }}</span>
+                    </div>
+                </div>
+                <div class="grid-row-1-col-1">
+                    <div class="form-combo">
+                        <label class="input-labels" for="years">Year</label>
+                        <input id="years" v-model="InputYears" type="number"/>
+                        <span id="yearsValidationError" class="validation-error">{{ YearsValidationError }}</span>
+                    </div>
+                </div>
+                <div class="grid-row-1-col-3">
+                    <div class="line"></div>
+                </div>
+                <div class="circle" @click="onSubmitForm"></div>
+                <div class="grid-row-1-col-3">
+                    <span class="timespan-item"><span class="number">{{ CalculatedDays }}</span> days</span>
+                </div>
+                <div class="grid-row-1-col-3">
+                    <span class="timespan-item"><span class="number">{{ CalculatedMonths }}</span> month</span>
+                </div>
+                <div class="grid-row-1-col-3">
+                    <span class="timespan-item"><span class="number">{{ CalculatedYears }}</span> years</span>
+                </div>
+            </div>
         </div>
-        <div class="grid-row-1-col-1">
-          <div class="form-combo">
-            <label for="months">Month</label>
-            <input v-model="InputMonths" type="number" id="months"/>
-          </div>
-        </div>
-        <div class="grid-row-1-col-1">
-          <div class="form-combo">
-            <label for="years">Year</label>
-            <input v-model="InputYears" type="number" id="years"/>
-          </div>
-        </div>
-        <div class="grid-row-1-col-3">
-          <div class="line"></div>
-        </div>
-        <div @click="onSubmitForm" class="circle"></div>
-        <div class="grid-row-1-col-3">
-            <span class="timespan-item"><span class="number">{{ CalculatedDays }}</span> days</span>
-        </div>
-        <div class="grid-row-1-col-3">
-          <span class="timespan-item"><span class="number">{{ CalculatedMonths }}</span> month</span>
-        </div>
-        <div class="grid-row-1-col-3">
-          <span class="timespan-item"><span class="number">{{ CalculatedYears }}</span> years</span>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <style scoped>
